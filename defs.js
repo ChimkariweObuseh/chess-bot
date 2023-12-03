939a60d546a39cb1cd23c08df6236aa10221856d
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-12-03 13:01:17 +0000

    Update defs.js

--- defs.js
+++ defs.js
@@ -1,1 +1,11 @@
+var PIECES = { EMPTY : 0, wP : 1, wN : 2, wB : 3, wR : 4, wQ : 5, wK : 6,
+              bP : 7, bN : 8, bB : 9, bR : 10, bQ : 11, bK : 12 };
 
+var BRD_SQ_NUM = 120;
+
+var FILES = { FILE_A:0, FILE_B:1, FILE_C:2, FILE_D:3,
+             FILE_E:4, FILE_F:5, FILE_G:6, FILE_H:7, FILE_NONE:8 };
+var RANKS = { RANK_1:0, RANK_2:1, RANK_3:2, RANK_4:3,
+             RANK_5:4, RANK_6:5, RANK_7:6, RANK_8:7, FILE_NONE:8 };
+
+var COLOURS = { WHITE : 0, BLACK : 1, BOTH : 2 }
