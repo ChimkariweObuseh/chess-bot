3b3577ae88b59ecfa3d6701ec2adaa95bfda1f09
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-12-03 21:23:28 +0000

    Update defs.js

--- defs.js
+++ defs.js
@@ -9,3 +9,18 @@ var RANKS = { RANK_1:0, RANK_2:1, RANK_3:2, RANK_4:3,
              RANK_5:4, RANK_6:5, RANK_7:6, RANK_8:7, FILE_NONE:8 };
 
 var COLOURS = { WHITE : 0, BLACK : 1, BOTH : 2 }
+
+var SQUARES = {
+  A1:21, B1:22, C1:23, D1:24, E1:25, F1:26, G1:27, H1:28,
+  A8:91, B8:92, C8:93, D8:94, E8:95, F8:96, G8:97, H8:98,
+  NO_SQ:99, OFFBOARD:100
+}
+
+var BOOL = { FALSE:0, TRUE:1 };
+
+var FilesBrd = new Array(BRD_SQ_NUM);
+var RanksBrd = new Array(BRD_SQ_NUM);
+
+function FR2SQ(f,r) {
+  return ( (21 + (f) ) + ( (r) * 10 );
+}
