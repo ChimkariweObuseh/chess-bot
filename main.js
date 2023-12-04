6dd8a7c6b4ff54638a7144d7352779c389ee2f23
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-12-04 08:36:30 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ $(function() {
   console.log("Main Init Called...?");
 });
 
+function InitFilesRanksBrd() {
+var index = 0;
+var file = FILES.FILE_A;
+var rank = RANKS.RANK_1;
+var sq = SQUARES.A1;
+}
+
 function init() {
   console.log("init called");
 }
