1a204d897edcaf13de580731e3b52fe1d0cd0a52
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-12-04 08:39:57 +0000

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ var index = 0;
 var file = FILES.FILE_A;
 var rank = RANKS.RANK_1;
 var sq = SQUARES.A1;
+
+  for (index = 0; index < BRD_SQ_NUM; ++index) {
+    FilesBrd[index] = SQUARES.OFFBOARD;
+    RanksBrd[index] = SQUARES.OFFBOARD;
+  }
 }
 
 function init() {
