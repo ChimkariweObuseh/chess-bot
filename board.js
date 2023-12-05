f83790edd09f035a1a1e9367ff90767b85874a07
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-12-05 14:30:10 +0000

    Delete board.js

--- board.js
+++
@@ -1,8 +0,0 @@
-var GameBoard = {};
-
-GameBoard.pieces = new Array(BRD_SQ_NUM);
-GameBoard.side = COLOUR.WHITE;
-GameBoard.fiftyMove = 0;
-GameBoard.hisPly = 0;
-GameBoard.ply = 0;
-GameBoard.castlePerm = 0;
