c22e36fa5191dfc6083990a2c779fb1b32db5b51
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2024-12-12 15:25:40 +0000

    Update defs.js

--- js/defs.js
+++ js/defs.js
@@ -1,4 +1,5 @@
-var PIECES =  { EMPTY : 0, wP : 1, wN : 2, wB : 3,wR : 4, wQ : 5, wK : 6, 
+PowerGlitch.glitch('.circle')
+	var PIECES =  { EMPTY : 0, wP : 1, wN : 2, wB : 3,wR : 4, wQ : 5, wK : 6, 
               bP : 7, bN : 8, bB : 9, bR : 10, bQ : 11, bK : 12  };
               
 var BRD_SQ_NUM = 120;
